90f37a57fecdfdfc4bb6752b92fb2aac1be8419c
Matt Schell <mschell@doitt.nyc.gov>
2020-02-18 16:25:33 -0500

    adds fixed rewire_view dunno where it went

--- src/main/sql/schema/definition/oracle/compile_rewire_view.sql
+++ src/main/sql/schema/definition/oracle/compile_rewire_view.sql
@@ -1,8 +1,8 @@
- create or replace PROCEDURE REWIRE_VIEW (
+CREATE OR REPLACE PROCEDURE GEOSERVER.REWIRE_VIEW (
    p_view                IN VARCHAR2
-) 
+)
 AUTHID CURRENT_USER AS
-   --Mschell! 
+   -- Mschell!
    -- rewire a view from blue to green or green to blue
    -- makes lazy but firm assumptions about naming conventions
    -- locked in throughout this project
@@ -11,8 +11,7 @@ AUTHID CURRENT_USER AS
    text_long            LONG; --ick
    new_select           VARCHAR2(12000);
    view_sql             VARCHAR2(12000);
-   current_table        VARCHAR2(64);
-   new_table            VARCHAR2(64);
+   new_color            VARCHAR2(1);
 BEGIN
     psql := 'SELECT LISTAGG(a.column_name, :p1) '
          || 'WITHIN GROUP (ORDER BY a.column_id) '
@@ -26,21 +25,27 @@ BEGIN
          || 'WHERE a.view_name = :p1 ';
     --includes the select
     EXECUTE IMMEDIATE psql INTO text_long USING UPPER(p_view);
-    IF UPPER(TO_CHAR(text_long)) LIKE ('%FROM%' || UPPER(p_view) || '_B')
+    --dbms_output.put_line('text long is ');
+    --dbms_output.put_line(text_long);
+    IF UPPER(TO_CHAR(text_long)) LIKE '%B'
     THEN
-        current_table := p_view || '_b';
-        new_table     := p_view || '_g';
+        --dbms_output.put_line('feeling blue!');
+        new_color := 'G';
     ELSE
-        current_table := p_view || '_g';
-        new_table     := p_view || '_b';
+        --dbms_output.put_line('feeling green!');
+        new_color := 'B';
     END IF;
-    new_select := REPLACE(UPPER(TO_CHAR(text_long))
-                         ,UPPER(current_table)
-                         ,UPPER(new_table));
-    EXECUTE IMMEDIATE 'create or replace view '
+    -- chomp off last char and replace with B or G
+    new_select := SUBSTR(UPPER(TO_CHAR(text_long))
+                         ,0
+                         ,LENGTH(UPPER(TO_CHAR(text_long))) - 1) || new_color;
+    --dbms_output.put_line('new select is ' || new_select);
+    psql :=  'create or replace force view '
                     || p_view || ' ('
                     || cols || ') AS '
                     || new_select;
+    --dbms_output.put_line(psql);
+    EXECUTE IMMEDIATE psql;
 END REWIRE_VIEW;
 /
 EXIT
